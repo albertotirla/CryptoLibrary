b14a5cab6711727c1cc3b4c5b34dd818f640587e
Alberto Tirla <albertotirla@gmail.com>
2022-10-22 12:53:35 +0300

    add initial base class for cifers

---
+++ Cifer.cs
@@ -0,0 +1,6 @@
+﻿namespace CryptoLibrary;
+public abstract class Cifer
+{
+public abstract string Encrypt<T>(string planeText, T key);
+public abstract string Decrypt<T>(string ciferText, T key);
+}
